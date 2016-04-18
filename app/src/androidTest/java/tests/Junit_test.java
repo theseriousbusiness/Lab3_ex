233e7a40c65258746c390d17cf9e11015a9c1771
emenon <mrmustachiojr@gmail.com>
2016-04-18 11:00:15 -0700

    First draft unit test (exercise 1)

---
+++ app/src/androidTest/java/tests/Junit_test.java
@@ -0,0 +1,54 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.rrivasfe.lab3_exercise.MainActivity;
+import com.example.rrivasfe.lab3_exercise.R;
+
+/**
+ * Created by rrivasfe on 4/18/2016.
+ */
+public class Junit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+    public Junit_test() {
+        super(MainActivity.class);
+        add_testrunner();
+
+    }
+
+    public void add_testrunner () {
+        add_test1(3,4); // positive ints
+        add_test1(-3,-4); // negative ints
+        add_test1(-67,234); // neg, pos ints
+        add_test1(234,-13); // pos, neg ints
+        add_test1(43.43, 65.3); // pos doubles
+        add_test1(0,0.0); // zeros
+        add_test1(0, -45.21); // zero, neg double
+        add_test1(6452, 0); // pos int, zero
+    }
+
+    public void add_test1(double a, double b) {
+
+        mainActivity = getActivity();
+
+        TextView tv1 = (TextView) mainActivity.findViewById(R.id.editText);
+        TextView tv2 = (TextView) mainActivity.findViewById(R.id.editText2);
+        TextView tv3 = (TextView) mainActivity.findViewById(R.id.editText3);
+        Button addBtn = (Button) mainActivity.findViewById(R.id.button);
+
+        tv1.setText(Double.toString(a));
+        tv2.setText(Double.toString(b));
+
+        addBtn.performClick();
+
+        double result = Double.parseDouble(tv3.getText().toString());
+        double ref = a + b;
+        assertEquals(result, ref);
+
+    }
+
+
+}
